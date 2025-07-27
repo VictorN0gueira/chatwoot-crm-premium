9a42f57851bf2ec50ca19fe855164442dc3bea1f
Victor Nogueira <128488435+VictorN0gueira@users.noreply.github.com>
2025-07-27 17:50:22 -0300

    Update 20250105005821_remove_not_null_from_captain_documents.rb

--- db/migrate/20250105005821_remove_not_null_from_captain_documents.rb
+++ db/migrate/20250105005821_remove_not_null_from_captain_documents.rb
@@ -1,5 +1,6 @@
 class RemoveNotNullFromCaptainDocuments < ActiveRecord::Migration[7.0]
   def change
-    change_column_null :captain_documents, :name, true
+    # A saltar esta migração porque a tabela 'captain_documents' não foi criada.
+    # change_column_null :captain_documents, :name, true
   end
 end
